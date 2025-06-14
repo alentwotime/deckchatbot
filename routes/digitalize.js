0af2750b6cb6685460fe8d7c102418a461d03009
alentwotime <icjalenz@gmail.com>
2025-06-13 22:40:16 -0500

    Improve drawing upload UX with drag-and-drop (#43)

--- routes/digitalize.js
+++ routes/digitalize.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const multer = require('multer');
-const { body } = require('express-validator');
 const { digitalizeDrawing } = require('../controllers/digitalizeController');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
-const validate = [
-  body('image').custom((_, { req }) => {
-    if (!req.file) {
-      throw new Error('Image file is required');
-    }
-    return true;
-  })
-];
-router.post('/', upload.single('image'), validate, digitalizeDrawing);
+router.post('/', upload.single('image'), digitalizeDrawing);
 
 module.exports = router;
